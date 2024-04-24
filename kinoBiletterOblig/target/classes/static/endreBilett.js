700a0115ae57f75546e4972d8da7fb0e732e3536
Adamsol1 <143609195+Adamsol1@users.noreply.github.com>
2024-04-24 15:12:02 +0200

    Validering endring
    La til inputvalidering for endre bilett

--- kinoBiletterOblig/target/classes/static/endreBilett.js
+++ kinoBiletterOblig/target/classes/static/endreBilett.js
@@ -34,6 +34,94 @@ function formaterFilm(film){ //function for å formatere film
 
 }
 
+function valideringBilett(bilett){ //validering for bilett
+    let godkjentValidering = true;
+    let antallSjekk = true;
+    //sjekker antall
+    if (bilett.antall === '' || isNaN(bilett.antall) || bilett.antall < 1){
+        antallSjekk = false;
+        //feilmelding
+        $("#antallValidering").html("Vennligst kjøp minst en bilett")
+    }
+    //definerer antall til bilett
+    else{
+        $("#antallValidering").html("");
+
+    }
+
+    //sjekker fornavn input
+    let fornavnSjekk = true;
+    if (bilett.fornavn.length === 0) {
+        fornavnSjekk = false;
+        //feilmelding
+        $("#fornavnValidering").html("Vennligst oppgi et fornavn")
+
+    }
+    //definerer fornavn til bilett
+    else {
+        $("#fornavnValidering").html("")
+
+    }
+    //sjekker etternavn input
+    let etternavnSjekk = true;
+    if (bilett.etternavn.length === 0) {
+        etternavnSjekk = false;
+        //feilmelding
+        $("#etternavnValidering").html("Vennligst oppgi etternavn");
+    }
+    //definerer etternavn til bilett
+    else {
+        $("#etternavnValidering").html("")
+
+
+    }
+    //sjekker telefonnr input
+    let telefonnrSjekk = true;
+    let telefonRegex = /^\d{8}$/; //Test for telefonnr
+    if (!telefonRegex.test(bilett.telefonnr) ) {
+        telefonnrSjekk = false;
+        //feilmelding
+        $("#telefonnrValidering").html("Vennligst oppgi godkjent telefonnr")
+    }
+    //definerer telefonnr til bilett
+    else {
+        $("#telefonnrValidering").html("")
+
+    }
+    //sjekker epost input
+    let epostSjekk = true;
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(bilett.epost) ) {
+        epostSjekk = false;
+        //feilmelding
+        $("#epostValidering").html("Vennligst oppgi en godkjent epost");
+
+
+    }
+    //definerer epost til bilett
+    else {
+        $("#epostValidering").html("");
+
+    }
+    let valgFilmSjekk = true;
+    let valgtFilm = $("#valgFilm").val();
+    if (!valgtFilm || valgtFilm === "Velg film"){
+        valgFilmSjekk = false;
+        //feilmelding
+        $("#filmValidering").html("Vennligst velg en film");
+    }
+    else {
+        $("#filmValidering").html("");
+    }
+
+    //Sjekker valideringen
+    if (antallSjekk  && fornavnSjekk && etternavnSjekk && telefonnrSjekk && epostSjekk && valgFilmSjekk){
+        return godkjentValidering;
+    }
+}
+
+
+
 function endreBilett(){
     const bilett = {
         id : $("#id").val(),
@@ -45,8 +133,11 @@ function endreBilett(){
         epost : $("#epost").val()
     };
 
+if (valideringBilett(bilett)){
+
 
     $.post("/endreEnBilett", bilett, function (){
         window.location.href = "index.html";
     })
+    }
 }
